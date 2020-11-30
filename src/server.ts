81db77b3753d799c3fce728cecb38df936e276d4
Rafael de Melo <rafael.pereira20@icloud.com>
2020-11-30 13:59:13 -0300

    Adc cors

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import upLoadConfig from './config/upload';
@@ -11,6 +12,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 //
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(upLoadConfig.directory));
 app.use(routes);
